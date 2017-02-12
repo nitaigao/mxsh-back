0354e0ceb69c16a228118aa99d4b309a5201a852
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-12 20:09:28 +0800

    compaint user

--- app/jobs/complaint_job.rb
+++ app/jobs/complaint_job.rb
@@ -12,7 +12,7 @@ class ComplaintJob < ApplicationJob
     #{user.email} has been disabled due to a compaint
     EOS
     
-    Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']).ping message, channel: "#support", username: "Bounce", icon_emoji: ":warning:"
+    Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']).ping message, channel: "#support", username: "Complaint", icon_emoji: ":warning:"
   end
 end
 
