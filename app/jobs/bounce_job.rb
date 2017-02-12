2afcd1c371c340c0ac52a264f1c96a2effffb2d2
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-12 12:46:34 +0800

    removed old queue

--- app/jobs/bounce_job.rb
+++ app/jobs/bounce_job.rb
@@ -1,20 +1,12 @@
 class BounceJob < ApplicationJob
   queue_as :bounces
 
-  NOTIFIER = Slack::Notifier.new ENV['SLACK_WEBHOOK_URL']
-
-  rescue_from ActiveJob::DeserializationError do |ex|
-    Shoryuken.logger.error ex
-    Shoryuken.logger.error ex.backtrace.join("\n")
-  end
-
   def perform(payload)
-    Shoryuken.logger.info payload
-    # message = JSON.parse(payload["Message"])
-    # recipients = message['bounce']['bouncedRecipients']
-    # recipients.each do |recipient|
-    #   disable recipient['emailAddress'].gsub('<', '').gsub('>', '')
-    # end
+    message = JSON.parse(payload["Message"])
+    recipients = message['bounce']['bouncedRecipients']
+    recipients.each do |recipient|
+      disable recipient['emailAddress'].gsub('<', '').gsub('>', '')
+    end
   end
 
   def disable(email)
@@ -27,7 +19,8 @@ class BounceJob < ApplicationJob
     message = <<-EOS
     #{user.email} has been disabled
     EOS
-    NOTIFIER.ping message, channel: "#support", username: "Bounce", icon_emoji: ":warning:"
+    
+    Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']).ping message, channel: "#support", username: "Bounce", icon_emoji: ":warning:"
   end
 end
 
