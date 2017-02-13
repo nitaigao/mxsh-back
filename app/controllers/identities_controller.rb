f1fb88930f5d4561a5f951e8d604dfd50599a9a0
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-13 09:54:45 +0800

    no need for that configuration of session storage

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -3,11 +3,13 @@ class IdentitiesController < ApplicationController
 
   def index
     @identities = current_user.identities
+    fresh_when(@identities)
   end
 
   def create
     hash = SecureRandom.hex(3)
     email = "#{hash}@mxsh.io"
     @identity = current_user.identities.create!(email: email)
+    fresh_when(@identity)
   end
 end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Rails.application.config.session_store :cookie_store, key: '_mxsh_session', expires: 7.days.from_now
