630834a5cc8a1856b4c42e081ee3fa8d2d55ec98
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-14 15:05:58 +0800

    removed authenticate call

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,10 +1,6 @@
 class IdentitiesController < ApplicationController
-  before_action :authenticate_user!
-
   def index
     @identities = Identity.where(user_id: current_user_id).order(created_at: :desc)
-    fresh_when @identities
-    expires_in 1.minute
   end
 
   def create
