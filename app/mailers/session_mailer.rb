95fb1acb158e55a63ed27498bba5223a0d26367e
Nicholas Kostelnik <nkostelnik@gmail.com>
2017-02-15 17:22:15 +0800

    use env var for email host

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -1,10 +1,8 @@
 class SessionMailer < ApplicationMailer
   default from: 'hello@mxsh.io'
   
-  HOST = Rails.env.development? ? 'http://mxsh.lvh.me:3000' : 'http://mxsh.io'
-
   def login(token)
-    @url = "#{HOST}/authorize/#{token.token}"
+    @url = "#{ENV['FRONTEND_HOST']}/authorize/#{token.token}"
     mail(to: token.email, subject: 'Login to MXSH')
   end
 end
